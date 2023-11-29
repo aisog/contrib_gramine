7a7cfee34c06c82f258d58f6766bf5a86c725a77
aisog <mrembas@seccon.pl>
2023-11-29 16:49:06 +0100

    fix meson version

--- Examples/aks-attestation/install_gramine.sh
+++ Examples/aks-attestation/install_gramine.sh
@@ -15,7 +15,7 @@ sudo apt-get install libsgx-dcap-quote-verify-dev
 sudo apt-get install -y build-essential \
     autoconf bison gawk nasm ninja-build pkg-config python3 python3-click \
     python3-jinja2 python3-pip python3-pyelftools wget
-sudo python3 -m pip install 'meson>=0.56' 'tomli>=1.1.0' 'tomli-w>=0.4.0'
+sudo python3 -m pip install  'meson>=0.56,<0.57' 'tomli>=1.1.0' 'tomli-w>=0.4.0'
 
 ##require pip3 install protobuf==3.20.3
 
