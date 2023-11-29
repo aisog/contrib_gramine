00e09dae78b0887677659422f138da033b387187
aisog <mrembas@seccon.pl>
2023-11-29 13:07:36 +0100

    fix2 mesos

--- Examples/aks-attestation/install_gramine.sh
+++ Examples/aks-attestation/install_gramine.sh
@@ -1,13 +1,16 @@
 sudo curl -fsSLo /usr/share/keyrings/gramine-keyring.gpg https://packages.gramineproject.io/gramine-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/gramine-keyring.gpg] https://packages.gramineproject.io/ stable main' | sudo tee /etc/apt/sources.list.d/gramine.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/gramine-keyring.gpg] https://packages.gramineproject.io/ $(lsb_release -sc) main" \
+| sudo tee /etc/apt/sources.list.d/gramine.list
 
-curl -fsSL https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | sudo apt-key add -
-echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/intel-sgx.list
-
-#echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo curl -fsSLo /usr/share/keyrings/intel-sgx-deb.asc https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/intel-sgx-deb.asc] https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -sc) main" \
+| sudo tee /etc/apt/sources.list.d/intel-sgx.list
 
 sudo apt-get update
-sudo apt-get install gramine-dcap # for out-of-tree DCAP driver (required by AKS Ubuntu 18.04 distro)
+sudo apt-get install gramine
+
+sudo apt-get update
+sudo apt-get install gramine # for out-of-tree DCAP driver (required by AKS Ubuntu 18.04 distro) current 1.4
 
 #sudo apt-get install libsgx-dcap-quote-verify-dev
 
@@ -15,7 +18,7 @@ sudo apt-get install gramine-dcap # for out-of-tree DCAP driver (required by AKS
 sudo apt-get install -y build-essential \
     autoconf bison gawk nasm ninja-build pkg-config python3 python3-click \
     python3-jinja2 python3-pip python3-pyelftools wget
-sudo python3 -m pip install 'meson>=0.56' 'tomli>=1.1.0' 'tomli-w>=0.4.0'
+sudo python3 -m pip install 'meson<=1.0.0' 'tomli>=1.1.0' 'tomli-w>=0.4.0'
 
 ##require pip3 install protobuf==3.20.3
 
